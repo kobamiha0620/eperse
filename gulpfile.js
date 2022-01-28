80774f31d6f3a9f887915c651ab8281497b83878
kobamiha <mihatu.irisawa@twinplanet.co.jp>
2022-01-28 19:38:03 +0900

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 // 各処理メソッドを、gulpの中で読み込むのではなく、直接取り出す
+// const {src, dest} = require('gulp');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sassGlob = require("gulp-sass-glob");
 
 gulp.task('sass', function(){
-  return gulp.src('styles/*.scss')
+  return gulp
+      .src('styles/*.scss')
+      .pipe(sassGlob())
       .pipe(sass())
+      .pipe(sass({outputStyle: 'compressed'}))
+      .pipe(sass({
+        includePaths: require('node-reset-scss').includePath
+      }))
       .pipe(gulp.dest('assets'))
-      .pipe(sass({outputStyle: 'expanded'}))
-});
+
+  });
+  
 
 
 gulp.task('watch', function(){

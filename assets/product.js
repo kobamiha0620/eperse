68e63a7479042397d59c911442bab52f6ab07f2d
kobamiha <mihatu.irisawa@twinplanet.co.jp>
2022-02-15 17:59:31 +0900

    mod slick

--- assets/product.js
+++ assets/product.js
@@ -15,24 +15,25 @@ $('.product__slick').slick({
     arrows: true,
     slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
     dots: true,　//下部ドットナビゲーションの表示
-    asNavFor: '.rip02__slick02',
+    // asNavFor: '.rip02__slick02',
     centerMode: true,
     speed: 300,
-    centerPadding: '19%'
+    centerPadding: '21%'
   });
 
 
-  $('.rip02__slick02').slick({
-    autoplay: false,//自動的に動き出すか。初期値はfalse。
-    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    slidesToShow: 1,//スライドを画面に3枚見せる
-    arrows: false,
-    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-    dots: false,　//下部ドットナビゲーションの表示
-    asNavFor: '.rip02__slick',
-    speed: 300,
-    centerMode: true
-  });
+  // $('.rip02__slick02').slick({
+  //   fade: true,
+  //   autoplay: false,//自動的に動き出すか。初期値はfalse。
+  //   infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  //   slidesToShow: 1,//スライドを画面に3枚見せる
+  //   arrows: false,
+  //   slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+  //   dots: false,　//下部ドットナビゲーションの表示
+  //   asNavFor: '.rip02__slick',
+  //   speed: 300,
+  //   centerMode: true
+  // });
 
 
 

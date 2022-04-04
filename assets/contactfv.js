8de244b3d0357bff8feaf9730b2d9c1da4f013e3
kobamiha <mihatu.irisawa@twinplanet.co.jp>
2022-04-04 12:58:21 +0900

    mod

--- assets/contactfv.js
+++ assets/contactfv.js
@@ -1,20 +1,46 @@
+//thumb
 var swiper = new Swiper(".mySwiper", {
-    loop: true,
-    spaceBetween: 3.5,
-    slidesPerView: 5,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".mySwiper2", {
-    loop: true,
-    spaceBetween: 0,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+  // デフォルトの設定
+  loop: false,
+  spaceBetween: 3.5,
+  slidesPerView: 5,
+  slidesPerGroup: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+  breakpointsBase:'product',
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // Swiper のコンテナ幅が 750px 以上の場合
+    750: {
+      slidesPerView: 5,
+      spaceBetween: 3.5,
+      slidesPerGroup: 5
+    }
+  },
+});
+
+//fv
+var swiper2 = new Swiper(".mySwiper2", {
+  breakpointsBase:'product',
+  // デフォルトの設定
+
+  loop: true,
+  spaceBetween: 0,
+  slidesPerView: 1,
+
+  breakpoints: {
+    // Swiper のコンテナ幅が 750px 以上の場合
+    750: {
+      slidesPerView: 1
+    }
+  },
+
+  thumbs: {
+    swiper: swiper,
+  },
+});
 
 
